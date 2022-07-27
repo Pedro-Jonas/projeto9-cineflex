0cf58ae4c6723a4ec363c54dc8c5aef45f36def3
jonas <pedrojonas.pj77@gmail.com>
2022-07-27 11:42:13 -0300

    Primeiro commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import "../assets/css/reset.css";
+import "../assets/css/style.css";
+
+export default function App(){
+    return(
+        <>
+        <div className="top">
+            CINEFLEX
+        </div> 
+        <div className="message">
+            Selecione o filme
+        </div>
+        </>
+    );
+};
