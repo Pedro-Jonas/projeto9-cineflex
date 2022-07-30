23103c7ec9f47c182a49aa2c407e35cbff2eab9a
jonas <pedrojonas.pj77@gmail.com>
2022-07-29 22:27:37 -0300

    mostrar cadeiras disponíveis e indisponíveis da sessão

--- src/components/Seats/Seats.js
+++ src/components/Seats/Seats.js
@@ -3,9 +3,11 @@ import Footer from '../Footer/Footer';
 import { useParams} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from "axios";
+import "./style.css"
 
 export default function Seats(){
     const {idSessao}= useParams();
+    const [data, setData] = useState([]);
     const [seats, setSeats] = useState([]);
     const [movie, setMovie] = useState([]);
     const [day, setDay] = useState([]);
@@ -15,15 +17,40 @@ export default function Seats(){
         `https://mock-api.driven.com.br/api/v7/cineflex/showtimes/${idSessao}/seats`
     );
     promise.then((respost)=> {
-    setSeats(respost.data) 
+    setData(respost.data)
+    setSeats(respost.data.seats) 
     setMovie(respost.data.movie)
     setDay(respost.data.day)
     });
     }, [idSessao]);
+
     return(
     <>
     <Message text="Selecione o(s) assento(s)"/>
-    <Footer url={movie.posterURL} title={movie.title} day={day.weekday} time={seats.name}/>
+    <div className="seats">
+        {seats.map((seat)=> {
+        const avaliable = seat.isAvailable
+        return(
+        avaliable?
+        (<div key={seat.id} className="seat disponivel">
+            {seat.name}
+        </div>) : 
+        (<div key={seat.id} className="seat indisponivel">
+            {seat.name}
+        </div>)
+       )})}
+    </div>
+    <div className="informacao">
+        <div className="seat selecionado"></div>
+        <div className="seat disponivel"></div>
+        <div className="seat indisponivel"></div>
+    </div>
+    <div className="descricao">
+       <p>selecionado</p> 
+       <p>disponível</p> 
+       <p>indisponível</p> 
+    </div>
+    <Footer url={movie.posterURL} title={movie.title} day={day.weekday} time={data.name}/>
     </>
     );
 ;}
